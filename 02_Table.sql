27175928219b2dba6a79f1fa4340e0c30c24610e
gr00shik <gr00shik@gmail.com>
2018-05-22 12:53:16 +0300

    recall syntaxis

--- 02_Table.sql
+++ 02_Table.sql
@@ -1,12 +1,12 @@
 use testdb;
 show tables;
-create table mytable(
+create table users(
 login varchar(20) not null unique,
 password varchar(20) not null default '12345',
 sex enum('M', 'F') not null default 'M',
 birthday date,
 primary key(login)
 );
-describe mytable;
-drop table mytable;
+describe users;
+drop table users;
 
